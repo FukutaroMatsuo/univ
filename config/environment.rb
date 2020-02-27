69b9bd461ce24fd93b2d1c4ec3297c50c5b9682e
Fukutaro Matsuo <54502796FukutaroMatsuo@users.noreply.github.com>
2020-02-27 11:26:49 +0000

    バリデーションとJavaScriptを使用したfadeoutの追加

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# 下記はバリデーション後のレイアウトが崩れないようにする設定
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
